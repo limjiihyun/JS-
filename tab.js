adc89dea2a520d24d0a50f7ade061763263b558b
limjiihuun <dlawlgus40@naver.com>
2023-04-24 21:56:12 +0900

    4/24

--- tab.js
+++ tab.js
@@ -1,11 +1,23 @@
-for (let i = 0; i < $('.tab-button').length ; i++){
+// for (let i = 0; i < $('.tab-button').length; i++){
 
-    $('.tab-button').eq(i).on('click', function(){
-      $('.tab-button').removeClass('orange');
-      $('.tab-button').eq(i).addClass('orange');
-      $('.tab-content').removeClass('show');
-      $('.tab-content').eq(i).addClass('show');
-    })
-};
+//   $('.tab-button').eq(i).on('click', function(){
+//     탭열기(i)
+//   })
+
+// };
+
+$('.list').click(function(e){
+  
+  탭열기(e.target.dataset.id)
+  
+})
+
+function 탭열기(구멍){
+  $('.tab-button').removeClass('orange');
+  $('.tab-button').eq(구멍).addClass('orange');
+  $('.tab-content').removeClass('show');
+  $('.tab-content').eq(구멍).addClass('show');
+}
+
 
   
